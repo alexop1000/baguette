e6151b2dc3f879482609a382de9a9df9923949c6
alexop1000 <46445843+alexop1000@users.noreply.github.com>
2019-01-21 19:58:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from discord.ext.commands import has_permissions
 
 startup_extensions = ['Music','serverinfo','autorole','fun','Mod','ModLog','welcomr','embed','translate','polls','level']
 Bot = discord.Client
-bot = commands.Bot('boi ')
+bot = commands.Bot('+')
 botdiscord = discord.Client()
 
 @bot.event
@@ -47,13 +47,13 @@ async def on_message(message):
         number = random.randint(5,10)
         await add_experience(users, message.author, number, message.server, servers)
         await level_up(users, message.author, message.channel, message.server, servers)
-                
+
     with open("storage/users.json", "w") as f:
         json.dump(users, f)
     with open("storage/Servers.json", "w") as k:
         json.dump(servers, k)
     await bot.process_commands(message)
-        
+
 
 async def update_data(users, user, server, servers):
     if not user.id + "-" + server.id in users:
@@ -80,9 +80,19 @@ async def level_up(users, user, channel, server, servers):
 
     if lvl_start < lvl_end:
         await bot.send_message(channel, ":tada: Congrats {}, you levelled up to level {}!".format(user.mention, lvl_end))
-        users[user.id + "-" + server.id]["level"] = lvl_end   
-        print("player {}/{} leveled up with {} xp".format(user.name, user.id, exp)) 
-    
+        users[user.id + "-" + server.id]["level"] = lvl_end
+        print("player {}/{} leveled up with {} xp".format(user.name, user.id, experience))
+
+@bot.command(pass_context=True)
+async def leaderboard(ctx):
+    t = open("storage/users.json", "r")
+    users = json.load(t)
+    experience = users[user.id + "-" + server.id]["experience"]
+    lvl_start = users[user.id + "-" + server.id]["level"]
+    high_score_list = sorted(users, key=lambda x : users[x].get('lvl_start', 0), reverse=True)
+    message = ''
+    for number, user in enumerate(high_score_list):
+        message += '{0}. {1} with {2}xp\n'.format(number + 1, user, user.id + "-" + server.id.get('lvl_start', 0))
 
 class Main_Commands():
     def __init__(self, bot):
