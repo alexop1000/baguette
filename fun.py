df38d0dbfa1b0265b0f76961747dc380fba7926a
alexop1000 <46445843+alexop1000@users.noreply.github.com>
2019-01-21 18:31:04 +0100

    Update fun.py

--- fun.py
+++ fun.py
@@ -163,9 +163,28 @@ class Fun:
         embed.set_footer(text="{} -> {}".format(ctx.message.author, self.get_time()))
         await self.bot.send_message(ctx.message.author, embed=embed)
 
-
     def get_time(self):
         return datetime.datetime.now().strftime("%X")
+
+    @commands.command(pass_context=True)
+    async def ownertest(self, ctx):
+        if ctx.message.author.id == "289463060178010123":
+            embed = discord.Embed(title="title ~~(did you know you can have markdown here too?)~~", colour=discord.Colour(0xe6a28d), url="https://discordapp.com", description="this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```", timestamp=datetime.datetime.utcfromtimestamp(1548091066))
+
+            embed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+            embed.set_author(name="author name", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+            embed.set_footer(text="footer text", icon_url="https://cdn.discordapp.com/embed/avatars/0.png")
+
+            embed.add_field(name="🤔", value="some of these properties have certain limits...")
+            embed.add_field(name="😱", value="try exceeding some of them!")
+            embed.add_field(name="🙄", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
+            embed.add_field(name="<:thonkang:219069250692841473>", value="these last two", inline=True)
+            embed.add_field(name="<:thonkang:219069250692841473>", value="are inline fields", inline=True)
+
+            await self.bot.say(content="this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", embed=embed)
+         else:
+            self.bot.say("Ehm how do you know this command? Oh well only the owner can use it")
     
 def check_folders():
     if not os.path.exists("data/useful"):
